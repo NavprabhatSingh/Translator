d2f9111930dd8d07f9a00257e2ed599e6bacfb15
NavprabhatSingh <navprabhatsingh252@gmail.com>
2025-04-04 18:54:07 +0530

    Added more languages

--- js/countries.js
+++ js/countries.js
@@ -1,9 +1,18 @@
-const countries={
-	"am-ET": "Amharic",
-	"bn-IN": "Bengali",
-	"hi-IN": "Hindi",
-	"en-US": "English",
-	"ko-KR": "Korean",
-	"pa-IN": "Punjabi",
-	"or-IN": "Oriya"
-}
+const countries = {
+    "en-US": "English",
+    "bn-IN": "Bengali",
+    "es-ES": "Spanish",
+    "fr-FR": "French",
+    "de-DE": "German",
+    "hi-IN": "Hindi",
+    "ja-JP": "Japanese",
+    "ko-KR": "Korean",
+    "pt-PT": "Portuguese",
+    "ru-RU": "Russian",
+    "zh-CN": "Chinese (Simplified)",
+    "ar-SA": "Arabic",
+    "it-IT": "Italian",
+    "tr-TR": "Turkish",
+    "vi-VN": "Vietnamese",
+    "th-TH": "Thai"
+};
